90a16b30bc844d869bf3262789fc9146593eee96
sharifuzzaman <sharifapuzaman@gmail.com>
2025-10-18 13:08:18 +0600

    User can type their own color hexadecimal code to the input field and change the background color

--- color.js
+++ color.js
@@ -4,11 +4,13 @@
  * - Also display the hex code to a disabled input field
  * - Add a button to copy the color code
  * -add a toast message when the color code is copied
+ * - Bonus: Add slide-in and slide-out animation to the toast message
+ * -User can type their own color hexadecimal code to the input field and change the background color
  */
 
 // Steps
 // global variable declaration
-let div=null;
+let div = null;
 
 // Step 1 - create onload handler
 window.onload = () => {
@@ -27,7 +29,7 @@ function main() {
 		const bgColor = generateHexColor();
 		root.style.backgroundColor = bgColor;
 		output.value = bgColor;
-		copyBtn.innerHTML = "Copy";
+		
 	});
 	// step 5 - handle the copy button click event
 	copyBtn.addEventListener('click', function () {
@@ -36,10 +38,23 @@ function main() {
 			div.remove();
 			div = null;
 		}
-
-		copyBtn.innerHTML = "Code Copied";
-		generateToastMessage(`${output.value} copied`);
+		// copyBtn.innerHTML = "Code Copied";
+		
+		else if (isValidHex(output.value)){
+			generateToastMessage(`${output.value} copied to clipboard`);
+		}else{
+			alert("Invalid Color Code");
+		}
 	});
+	// step 10-implement change handler on input field
+	output.addEventListener('keyup',function(e){
+		const color = e.target.value;
+		if (color && isValidHex(color)) {
+			root.style.backgroundColor=color;		
+		}
+	});		
+
+
 }
 
 // step 2 - random color generator function
@@ -54,27 +69,47 @@ function generateHexColor() {
 }
 // step 7-create a dynamic toast message
 function generateToastMessage(msg) {
-	div=document.createElement("div");
-	div.innerText=msg;
-	div.className='toast-message toast-message-slide-in';
-	div.addEventListener('click',function(){
+	div = document.createElement("div");
+	div.innerText = msg;
+	div.className = 'toast-message toast-message-slide-in';
+	div.addEventListener('click', function () {
 		div.classList.remove('toast-message-slide-in');
 		div.classList.add('toast-message-slide-out');
-	// step 8-clear the toast message after animation end
-	div.addEventListener('animationend',function(){
-		div.remove()
-		div=null;
-	})
+		// step 8-clear the toast message after animation end
+		div.addEventListener('animationend', function () {
+			div.remove()
+			div = null;
+		})
 
 	})
-	
+
 
 
 	document.body.appendChild(div);
 
 
 }
+/**
+ * 
+ * @param {string} color 
+ */
+// step 9- create isHexValid function 
+function isValidHex(color) {
+	if (color.length !== 7) return false;
+	if (color[0] !== '#') return false;
 
-// step 6-active toast message
-// step 7-create a dynamic toast message
-// step 8-clear the toast message after animation end
+	color=color.substring(1)
+	return /^[0-9A-Fa-f]{6}$/i.test(color);
+}
+	
+
+
+
+
+
+
+
+
+
+
+
