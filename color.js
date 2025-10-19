5afa384bc4d2962328b70eab16e54a7b74d91fff
sharifuzzaman-apu <sharifapuzaman@gmail.com>
2025-10-19 14:43:36 +0600

    Refactor color.js for improved functionality
    Refactor color generation and event handling logic. Implement features for random color generation, user input handling, and toast notifications.

--- color.js
+++ color.js
@@ -1,171 +1,119 @@
+/**
+ * Project Requirements:
+ * -Change the background color by generating random hex color by clicking a button
+ * -Also display the hex code to a disabled input field
+ * -Add a button to copy the color code
+ * -add a toast message when the color code is copied
+ * -Bonus: Add slide-in and slide-out animation to the toast message
+ * -User can type their own color hexadecimal code to the input field and change the background color
+ * -# is including in the input field
+ * -Lower case to upper case conversion of hex code in the input field
+ * -show rgb color too,but do not need to edit it
+ * user can also copy the rgb color code
+ */
 
-// globals
-let toastContainer = null;
-const defaultColor={
-	red:221,
-	green:222,
-	blue:238
-}
+// Steps
 
-// onload handler
+// global variable declaration
+
+let div = null;
+
+// Step 1 - create onload handler
+
 window.onload = () => {
 
 	main();
-	updateColorCodeToDom(defaultColor)
 };
-// main or boot function,this function of take care of getting all DOM reference
 
 function main() {
-	// dom references
-	const generateRandomColorBtn = document.getElementById('generate-random-color')
-	const colorModeHexInp = document.getElementById('input-hex')
-	const colorSliderRed = document.getElementById('color-slider-red')
-	const colorSliderGreen = document.getElementById('color-slider-green')
-	const colorSliderBlue = document.getElementById('color-slider-blue')
 
-	const copyToClipBoardBtn = document.getElementById('copy-to-clipboard')
+	// step 3 - collect all necessary references
+
+	const root = document.getElementById('root');
+	const output = document.getElementById('output');
+	const output2 = document.getElementById('output2');
+	const changeBtn = document.getElementById('change-btn');
+	const copyBtn = document.getElementById('copy-btn');
+	const copyBtn2 = document.getElementById('copy-btn2');
 
 
+	// step 4 - handle the change button click event
+	// step 13-update color code to display the rgb colors
 
-	// event listeners
-	generateRandomColorBtn.addEventListener('click', handleGenerateRandomColorBtn);
-	colorModeHexInp.addEventListener('keyup', handleColorModeHexInp);
-	colorSliderRed.addEventListener('change', handleColorSliders(colorSliderRed, colorSliderGreen, colorSliderBlue))
-	colorSliderGreen.addEventListener('change', handleColorSliders(colorSliderRed, colorSliderGreen, colorSliderBlue))
-	colorSliderBlue.addEventListener('change', handleColorSliders(colorSliderRed, colorSliderGreen, colorSliderBlue))
+	changeBtn.addEventListener('click', function () {
 
-	copyToClipBoardBtn.addEventListener('click', handleCopyToClipboard)
+		const color = generateDecimalColor();
+		const hex = generateHexColor(color);
+		const rgb = generateRGBColor(color);
+		root.style.backgroundColor = hex;
+		output.value = hex.substring(1);
+		output2.value = rgb;
 
-}
+	});
 
-// event handlers
-function handleGenerateRandomColorBtn() {
+	// step 5 - handle the copy button click event
 
-	const color = generateDecimalColor();
-	updateColorCodeToDom(color)
-}
+	copyBtn.addEventListener('click', function () {
 
-function handleColorModeHexInp(e) {
-	const hexColor = e.target.value;
-	if (hexColor) {
-		this.value = hexColor.toUpperCase();
-		if (isValidHex(hexColor)) {
-			const color = hexToDecimalColor(hexColor)
-			updateColorCodeToDom(color)
+		navigator.clipboard.writeText(`#${output.value}`);
+
+		if (div !== null) {
+			div.remove();
+			div = null;
 		}
-	}
-}
 
-function handleColorSliders(colorSliderRed, colorSliderGreen, colorSliderBlue) {
+		// copyBtn.innerHTML = "Code Copied";
+		// step 11 - prevent copying hex code if it is not valid
 
-	return function () {
-		const color = {
-			red: parseInt(colorSliderRed.value),
-			green: parseInt(colorSliderGreen.value),
-			blue: parseInt(colorSliderBlue.value)
-		}
-		updateColorCodeToDom(color)
-	}
-}
-
-function handleCopyToClipboard() {
-	const colorModeRadio = document.getElementsByName('color-mode')
-	const mode = getCheckedValueFromRadios(colorModeRadio)
-	if (mode === null) {
-		throw new Error('invalid radio input')
-	}
-	if (toastContainer !== null) {
-		toastContainer.remove()
-		toastContainer = null
-	}
-	if (mode === 'hex') {
-		const hexColor = document.getElementById('input-hex').value
-		if (hexColor && isValidHex(hexColor)) {
-			navigator.clipboard.writeText(`#${hexColor}`)
-			generateToastMessage(`#${hexColor} copied`)
+		else if (isValidHex(output.value)) {
+			generateToastMessage(`#${output.value} copied to clipboard`);
 		} else {
-			alert('Invalid HEX Code')
+			alert("Invalid Color Code");
+		}
+	});
+
+	// step 17-implement copy function
+	
+	copyBtn2.addEventListener('click', function () {
+
+		navigator.clipboard.writeText(`#${output2.value}`);
+
+		if (div !== null) {
+			div.remove();
+			div = null;
 		}
 
+		// copyBtn.innerHTML = "Code Copied";
+		// step 11 - prevent copying hex code if it is not valid
 
-	} else {
-		const rgbColor = document.getElementById('input-rgb').value
-		if (rgbColor) {
-			navigator.clipboard.writeText(`${rgbColor}`)
-			generateToastMessage(`${rgbColor} copied`)
+		else if (isValidHex(output.value)) {
+			generateToastMessage(`#${output2.value} copied to clipboard`);
 		} else {
-			alert('Invalid RGB Code')
+			alert("Invalid Color Code");
 		}
+	});
 
-	}
+	// step 10-implement change handler on input field
 
-}
+	output.addEventListener('keyup', function (e) {
+		const color = e.target.value;
+		if (color) {
+			output.value = color.toUpperCase();
+			if (isValidHex(color)) {
+				root.style.backgroundColor = `#${color}`;
 
-// DOM Functions
-
-/**
- * generate a dynamic DOM element to show a toast message
- * @param {string} msg 
- */
-function generateToastMessage(msg) {
-	toastContainer = document.createElement("div");
-	toastContainer.innerText = msg;
-	toastContainer.className = 'toast-message toast-message-slide-in';
-	toastContainer.addEventListener('click', function () {
-		toastContainer.classList.remove('toast-message-slide-in');
-		toastContainer.classList.add('toast-message-slide-out');
-		// step 8-clear the toast message after animation end
-		toastContainer.addEventListener('animationend', function () {
-			toastContainer.remove()
-			toastContainer = null;
-		})
-
-	})
-	document.body.appendChild(toastContainer);
-}
-
-/**
- * find the checked elements from a list of radio buttons
- * @param {array} radioNodeList 
- * @returns {string/null}
- */
-function getCheckedValueFromRadios(nodes) {
-	let checkedValue = null
-	for (let i = 0; i < nodes.length; i++) {
-		if (nodes[i].checked) {
-			checkedValue = nodes[i].value
-			break;
+				// step 16-update change handler
+				output2.value=hexToRGB(color);
+			}
 		}
-	}
-	return checkedValue;
+	});
+
 
 }
-/**
- * update dom with calculated color values
- */
-function updateColorCodeToDom(color) {
-	const hexColor = generateHexColor(color)
-	const rgbColor = generateRGBColor(color)
 
-	document.getElementById('color-display').style.backgroundColor = `#${hexColor}`
-	document.getElementById('input-hex').value = hexColor
-	document.getElementById('input-rgb').value = rgbColor
-	document.getElementById('color-slider-red').value = color.red
-	document.getElementById('color-slider-red-label').innerText = color.red
-	document.getElementById('color-slider-green').value = color.green
-	document.getElementById('color-slider-green-label').innerText = color.green
-	document.getElementById('color-slider-blue').value = color.blue
-	document.getElementById('color-slider-blue-label').innerText = color.blue
-}
-// create function of add
-
-
-// utility functions
-
-/**
- * function-1-generate three random decimal number for red,green,blue.return as an object
- * @returns 
- */
+// 
+// step 12-refactor the color generator function
+// function-1-generate three random decimal number for red,green,blue.return as an object
 
 function generateDecimalColor() {
 	const red = Math.floor(Math.random() * 255);
@@ -179,13 +127,14 @@ function generateDecimalColor() {
 
 }
 
-/**
- * function-2 generate hex color from decimal color
- * @param {*} param0 
- * @returns 
- */
+// function-2 generate hex color from decimal color
 
 function generateHexColor({ red, green, blue }) {
+
+	// const {red,green,blue}=generateDecimalColor();
+	// const twoCodeRed=red<=9?`0${red}`:red.toString(16);
+	// const twoCodeGreen=green<=9?`0${green}`:green.toString(16);
+	// const twoCodeBlue=blue<=9?`0${blue}`:blue.toString(16);
 
 	const getTwoCode = (value) => {
 		const hex = value.toString(16);
@@ -193,52 +142,78 @@ function generateHexColor({ red, green, blue }) {
 	}
 
 
-	return `${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(blue)}`.toUpperCase();
+	return `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(blue)}`.toUpperCase();
 }
 
 
-/**
- * function-3 generate rgb color from decimal color
- * @param {*} param0 
- * @returns 
- */
+// function-3 generate rgb color from decimal color
 
 function generateRGBColor({ red, green, blue }) {
 
 	return `rgb(${red}, ${green}, ${blue})`;
 }
 
+
+// step 15-convert hex color to rgb color
 /**
- * function-4 convert hex to decimal color object
- * @param {object} 
+ * convert hex to rgb
+ * @param {string} hex 
  */
-function hexToDecimalColor(hex) {
-	const red = parseInt(hex.slice(0, 2), 16);
-	const green = parseInt(hex.slice(2, 4), 16);
-	const blue = parseInt(hex.slice(4, 6), 16);
-	return {
-		red,
-		green,
-		blue
-	};
+function hexToRGB(hex) {
+	const red=parseInt(hex.slice(0,2),16);
+	const green=parseInt(hex.slice(2,4),16);
+	const blue=parseInt(hex.slice(4,6),16);
+	return `rgb(${red}, ${green}, ${blue})`;
 
-
-
+	
 
 }
 
+// step 2 - random color generator function
+// function generateHexColor() {
+// 	// #000000 #ffffff
+// 	// 255, 255, 255 -> #FFFFFF
+// 	const red = Math.floor(Math.random() * 255);
+// 	const green = Math.floor(Math.random() * 255);
+// 	const blue = Math.floor(Math.random() * 255);
+
+// 	return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+// }
+
+// step 7-create a dynamic toast message
+
+function generateToastMessage(msg) {
+	div = document.createElement("div");
+	div.innerText = msg;
+	div.className = 'toast-message toast-message-slide-in';
+	div.addEventListener('click', function () {
+		div.classList.remove('toast-message-slide-in');
+		div.classList.add('toast-message-slide-out');
+		// step 8-clear the toast message after animation end
+		div.addEventListener('animationend', function () {
+			div.remove()
+			div = null;
+		})
+
+	})
+	document.body.appendChild(div);
+}
+
+
+
+
 /**
- * function-4 validate hex color code
+ * 
  * @param {string} color 
  */
-
+// step 9- create isHexValid function 
 
 function isValidHex(color) {
 
 	if (color.length !== 6) return false;
 	return /^[0-9A-Fa-f]{6}$/i.test(color);
 }
-
+//
 
 
 
